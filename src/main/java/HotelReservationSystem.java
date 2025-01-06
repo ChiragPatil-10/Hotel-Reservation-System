47dd0ae1e053f5ecaa52b96d7ccf175d33ec9524
Chirag Patil <patilchirag192k@gmail.com>
2025-01-06 14:03:07 +0530

    Add hotel names

--- src/main/java/HotelReservationSystem.java
+++ src/main/java/HotelReservationSystem.java
@@ -1,5 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Hotel {
+    String name;
+    int rating;
+    int weekDayRegular;
+    int weekDayRewards;
+    int weekendRateRegular;
+    int weekendRateRewards;
+
+    public Hotel(String name, int rating,int weekDayRegular,int weekDayRewards,int weekendRateRegular, int weekendRateRewards) {
+        this.name = name;
+        this.rating = rating;
+        this.weekDayRegular = weekDayRegular;
+        this.weekDayRewards = weekDayRewards;
+        this.weekendRateRegular = weekendRateRegular;
+        this.weekendRateRewards = weekendRateRewards;
+    }
+}
+
+class HotelReservation {
+    List<Hotel> hotels;
+
+    public HotelReservation(){
+        hotels = new ArrayList<>();
+    }
+
+    public void addHotel(String name, int rating, int weekdayRateRegular, int weekdayRateReward, int weekendRateRegular, int weekendRateReward) {
+        Hotel newHotel = new Hotel(name, rating, weekdayRateRegular, weekdayRateReward, weekendRateRegular, weekendRateReward);
+        hotels.add(newHotel);
+    }
+}
+
 public class HotelReservationSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
+        Scanner sc = new Scanner(System.in);
+        HotelReservation reservationSystem = new HotelReservation();
+
+        System.out.println("Do you want to add new Hotel? (Yes/No)");
+        String addHotelResponse = sc.nextLine().toLowerCase();
+
+        if(addHotelResponse.equalsIgnoreCase("yes")) {
+            System.out.println("Enter hotel name: ");
+            String name = sc.nextLine();
+            System.out.println("Enter hotel rating: ");
+            int rating = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter weekday rate for regular customer: ");
+            int weekdayRateRegular = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter weekday rate for reward customer: ");
+            int weekdayRateReward = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter weekend rate for regular customer: ");
+            int weekendRateRegular = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter weekend rate for reward customer: ");
+            int weekendRateReward = Integer.parseInt(sc.nextLine());
+
+            reservationSystem.addHotel(name, rating, weekdayRateRegular, weekdayRateReward, weekendRateRegular, weekendRateReward);
+            System.out.println("Hotel added successfully");
+        }
+
     }
 }
--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -1,3 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelReservationTest {
 
+    @Test
+    public void testAddHotel() {
+        HotelReservation reservationSystem = new HotelReservation();
+        reservationSystem.addHotel("Hotel Orchid",4,150,100,80,60);
+
+        assertEquals(1, reservationSystem.hotels.size());
+        assertEquals("Hotel Orchid", reservationSystem.hotels.get(0).name);
+        assertEquals(4,reservationSystem.hotels.get(0).rating);
+    }
 }
